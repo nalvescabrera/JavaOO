2f9b81b3634d1400236e11ba8b134d593cf42648
Natalia Cabrera <n.alvescabrera@gmail.comm>
2021-05-19 18:14:11 -0300

    Adicionando classes Conta e TestaMetodo

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -8,8 +8,27 @@ public class Conta {
 	int numero;
 	String titular;
 	
-	void deposita(double valor) {
+	//Metodos
+	public void deposita(double valor) {
 		this.saldo = this.saldo + valor;
 	}
 
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor,Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -=valor;
+			destino.deposita(valor);
+			return true;
+		}
+			return false;
+		
+	}
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -7,6 +7,24 @@ public class TestaMetodo {
 		conta.saldo = 100;
 		conta.deposita(50);
 		System.out.println(conta.saldo);
+		
+		boolean conseguiuRetirar = conta.saca(20);
+		System.out.println(conta.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		boolean sucessoTransferencia = contaDaMarcela.transfere(3000,conta);
+		if(sucessoTransferencia) {
+		System.out.println("Transferencia com sucesso");
+		}else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(conta.saldo);
+		
+		conta.titular = "Natalia Cabrera";
+		System.out.println(conta.titular);
 	}
 
 }
